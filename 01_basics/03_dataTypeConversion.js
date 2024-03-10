02ef72d14a5ec0f7d78530a0505545fd3af4143a
rishi <rishirajneupane0125@gmail.com>
2024-03-11 04:49:15 +0545

    Conversion of datatypes

---
+++ 01_basics/03_dataTypeConversion.js
@@ -0,0 +1,30 @@
+// let score="33abc"
+// let score=true
+// let score="Rishi"
+// let score= null
+// let score= undefined
+
+/* "33" ==> 33
+"33abc"==>NaN
+true==>1  /false==>0
+*/
+
+
+// console.log(typeof score)
+// console.log(typeof (score))//another way 
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn=1
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/* 1==> true /0==>false
+""==> false
+"rishi"==>true  */
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
