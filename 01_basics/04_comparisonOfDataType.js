41c64c73c3dc2f273132151d53f6df79520a22bd
rishi <rishirajneupane0125@gmail.com>
2024-03-11 05:42:41 +0545

    Comparison of Datatypes

---
+++ 01_basics/04_comparisonOfDataType.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(1<2);
+// console.log(2==1);
+// console.log(2!=8);
+// console.log(2>=1);
+// console.log(8<=10);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null>=0);
+// console.log(null==0);
+
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+
+console.log("2"===2);  // it checks its datatypes also 
