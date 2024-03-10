4d5842fcae63a90af54e03a3869f9b8d1b341f9c
rishi <rishirajneupane0125@gmail.com>
2024-03-11 05:21:54 +0545

    various  operation

--- 01_basics/03_dataTypeConversion.js
+++ 01_basics/03_dataTypeConversion.js
@@ -26,5 +26,47 @@ true==>1  /false==>0
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+/* **************operations  ********************* */
+
+
+let value=3
+negValue= -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(3*2);
+// console.log(5-2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1="Rishi"
+let str2=" Raj"
+ let str3= str1+str2
+// console.log(str3);
+
+
+// console.log("2"+1);
+// console.log(1+"2");
+// console.log("2"+1+5);
+// console.log(1+5+"2");
+// console.log(1+"5"+2);
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2 , num3
+num1=num2=num3=2+2
+// console.log(num1);
+
+let gameCounter=100
+// ++gameCounter
+gameCounter++
+console.log(gameCounter);
