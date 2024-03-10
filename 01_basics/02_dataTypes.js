bb7948411641ab0d3624cf4caa2c354cbe39a216
Rishi Raj Neupane <98184879+rishirajneupane@users.noreply.github.com>
2024-03-10 22:21:16 +0000

    rename file

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";     // treat all js code as newer version
+// alert(3+3)    //   we are  using node.js not browser 
+/*
+..............primitive  datatype ..........
+number ==> 2 to power 53
+bigint 
+string==> ""
+boolean  ==> true/false
+null==> type:object // used  to assign value ,standalone value  eg temperature  value null return while server error etc
+undefied==> type undefined // not  defined /given 
+symbol==> unique
+
+
+.......
+
+object 
+
+*/
+console.log (typeof null)
