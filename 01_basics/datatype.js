5b8cfee25e4c735a37e2bf98e1135bf6a40a21b0
Rishi Raj Neupane <98184879+rishirajneupane@users.noreply.github.com>
2024-03-10 22:14:30 +0000

    datatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,18 @@
+"use strict";     // treat all js code as newer version
+// alert(3+3)    //   we are  using node.js not browser 
+/*
+..............primitive  datatype ..........
+number ==> 2 to power 53
+bigint 
+string==> ""
+boolean  ==> true/false
+null==> type:object // used  to assign value ,standalone value  eg temperature  value null return while server error etc
+undefied==> type undefined // not  defined /given 
+symbol==> unique
+
+
+.......
+
+object 
+
+
