e99e2d18a1c1c0bf114dc160d776c7786239d6ec
rishi <rishirajneupane0125@gmail.com>
2024-03-12 20:11:50 +0545

    switch case

---
+++ 03_advanced/04_switchCase.js
@@ -0,0 +1,33 @@
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+// const month=2
+// switch (month){
+//     case 1:console.log("jan");
+//     break;
+//     case 2:console.log("feb");
+//     break;
+//     case 3:console.log("march");
+//     break;
+//     default:console.log("default");
+//       break;
+// }
+
+const month="jan"
+switch (month){
+    case "jan":console.log("jan");
+    break;
+    case "feb":console.log("feb");
+    break;
+    case "march":console.log("march");
+    break;
+    default:console.log("default");
+      break;
+}
