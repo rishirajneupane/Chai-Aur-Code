48554b4b1dcf9145fc19f18b19f73e59428c4873
rishi <rishirajneupane0125@gmail.com>
2024-03-15 10:25:12 +0545

    keyboard

---
+++ keyboardCheck/script.js
@@ -0,0 +1,16 @@
+const insert = document.getElementById("insert");
+window.addEventListener("keydown", (e) => {
+  insert.innerHTML = ` <table>
+  <tr>
+    <th>key</th>
+    <th>keyCode</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key === ' ' ? 'Space' : e.key} </td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+  
+</table>`;
+});
